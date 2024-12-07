1810088448b2d7e7cf11ab6749b6f9c3a070533d
jaeggi <chr.jaeggi@gmail.com>
2024-12-07 22:01:11 +0100

    Day6 fuck up ... (don't know what's wrong in second part)

--- src/days/Day6.kt
+++ src/days/Day6.kt
@@ -1,9 +1,84 @@
 package days
 
-class Day6 {
+import utils.*
 
+class Day6 {
+    private val patrolRoom =
+        Array(numberOfLinesPerFile(6)) { CharArray(numberOfCharsPerLine(6)) }
+    private val initialPatrolRoute = mutableListOf<Point2D>()
+    private val createdObstacles = mutableListOf<Point2D>()
     fun solve() {
 
+        execFileByLineIndexed(6) { index, line ->
+            line.forEachIndexed { i, v ->
+                patrolRoom[index][i] = v
+            }
+        }
+        val start = patrolRoom.find('^')?.copy(d = Direction.N)
+        start?.let {
+            patrolAround(start, true, null, patrolRoom)
+        }
+        println(initialPatrolRoute.distinctBy { listOf(it.x, it.y) }.count())
+        findLoops()
+        println(createdObstacles.distinctBy { listOf(it.x, it.y) }.count())
     }
 
+    private tailrec fun patrolAround(
+        pos: Point2D,
+        initialPatrol: Boolean,
+        visited: MutableList<Point2D>?,
+        room: Array<CharArray>,
+    ): Boolean {
+        var current = pos
+        if (initialPatrol) initialPatrolRoute.add(current)
+        if (visited != null) {
+            if (current in visited) {
+                return true
+            }
+            visited += current
+        }
+
+        if (!room.inBounds(current.next)) {
+            return false
+        }
+
+        if (room.obstacleAhead(current.next)) {
+            current = current.copy(d = turn90degrees(current.d))
+        }
+
+        return patrolAround(current.next, initialPatrol, visited, room)
+    }
+
+    private fun findLoops() {
+        initialPatrolRoute.forEachIndexed { index, it ->
+            if (index > 0) {
+                val clonedPatrolRoom =
+                    Array(numberOfLinesPerFile(6)) { CharArray(numberOfCharsPerLine(6)) }
+                execFileByLineIndexed(6) { y, line ->
+                    line.forEachIndexed { x, v ->
+                        clonedPatrolRoom[y][x] = v
+                    }
+                }
+                    clonedPatrolRoom[it.y][it.x] = '#'
+                    val loopList = mutableListOf<Point2D>()
+                    if (patrolAround(initialPatrolRoute[0], false, loopList, clonedPatrolRoom)) {
+                        createdObstacles.add(it)
+                    }
+            }
+        }
+    }
+
+    private fun Array<CharArray>.obstacleAhead(p: Point2D) = this.at(p) == '#'
+
+    private fun turn90degrees(direction: Direction): Direction {
+        return when (direction) {
+            Direction.N -> Direction.E
+            Direction.E -> Direction.S
+            Direction.S -> Direction.W
+            Direction.W -> Direction.N
+            else -> {
+                throw IllegalStateException()
+            }
+        }
+    }
 }
--- src/utils/Point2D.kt
+++ src/utils/Point2D.kt
@@ -3,15 +3,17 @@ package utils
 data class Point2D(
     val x: Int,
     val y: Int,
+    val d: Direction,
 ) {
-    val n get() = this+Direction.N
-    val e get() = this+Direction.E
-    val s get() = this+Direction.S
-    val w get() = this+Direction.W
+    val n get() = this + Direction.N
+    val e get() = this + Direction.E
+    val s get() = this + Direction.S
+    val w get() = this + Direction.W
     val ne get() = this + Direction.NE
     val se get() = this + Direction.SE
     val sw get() = this + Direction.SW
     val nw get() = this + Direction.NW
+    val next get() = this + d
 }
 
 enum class Direction {
@@ -20,14 +22,14 @@ enum class Direction {
 
 operator fun Point2D.plus(direction: Direction): Point2D {
     return when (direction) {
-        Direction.N -> Point2D(x, y - 1)
-        Direction.E -> Point2D(x + 1, y)
-        Direction.S -> Point2D(x, y + 1)
-        Direction.W -> Point2D(x - 1, y)
-        Direction.NE -> Point2D(x + 1, y - 1)
-        Direction.SE -> Point2D(x + 1, y + 1)
-        Direction.SW -> Point2D(x - 1, y + 1)
-        Direction.NW -> Point2D(x - 1, y - 1)
+        Direction.N -> Point2D(x, y - 1, Direction.N)
+        Direction.E -> Point2D(x + 1, y, Direction.E)
+        Direction.S -> Point2D(x, y + 1, Direction.S)
+        Direction.W -> Point2D(x - 1, y, Direction.W)
+        Direction.NE -> Point2D(x + 1, y - 1, Direction.NE)
+        Direction.SE -> Point2D(x + 1, y + 1, Direction.SE)
+        Direction.SW -> Point2D(x - 1, y + 1, Direction.SW)
+        Direction.NW -> Point2D(x - 1, y - 1, Direction.NW)
     }
 }
 
@@ -36,3 +38,22 @@ fun Array<CharArray>.inBounds(p: Point2D): Boolean {
 }
 
 fun Array<CharArray>.at(p: Point2D): Char = this[p.y][p.x]
+
+fun Array<CharArray>.find(c: Char): Point2D? {
+    this.forEachIndexed { y, line ->
+        line.forEachIndexed { x, v ->
+            if (v == c) return Point2D(x, y, Direction.N)
+        }
+    }
+    return null
+}
+
+fun Array<CharArray>.count(c: Char): Int {
+    var res = 0
+    this.forEachIndexed { y, line ->
+        line.forEachIndexed { x, v ->
+            if (v == c) res++
+        }
+    }
+    return res
+}
