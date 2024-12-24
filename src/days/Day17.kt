114b1a249d516084b9c8ee5fba1b9193c0aa049f
jaeggi <chr.jaeggi@gmail.com>
2024-12-24 13:55:16 +0100

    Day17 copy/paste from : https://pastebin.com/raw/7DC2mf9g

--- src/days/Day17.kt
+++ src/days/Day17.kt
@@ -2,10 +2,6 @@ package days
 
 
 class Day17 {
-    private var regA = 30118712UL
-    private var regB = 0UL
-    private var regC = 0UL
-    private val program = listOf(2, 4, 1, 3, 7, 5, 4, 2, 0, 3, 1, 5, 5, 5, 3, 0)
 
     fun solve() {
         part1()
@@ -13,9 +9,10 @@ class Day17 {
     }
 
     private fun part1() {
-        val outCode = mutableListOf<ULong>()
-        runProgram(outCode)
-        println(outCode)
+        val regA = 30118712L
+        val program = listOf(2L, 4L, 1L, 3L, 7L, 5L, 4L, 2L, 0L, 3L, 1L, 5L, 5L, 5L, 3L, 0L)
+        val out = runProgram(program, regA)
+        println(out)
     }
 
 
@@ -23,40 +20,64 @@ class Day17 {
         // 2, 4, 1, 3, 7, 5, 4, 2, 0, 3, 1, 5, 5, 5, 3, 0
         // last output defines first input of A in octal
         // https://www.reddit.com/r/adventofcode/comments/1hg38ah/comment/m2gyonb/
-        // 236555995274861UL should be the solution :(
+        // https://pastebin.com/raw/7DC2mf9g
+        val program = listOf(2L, 4L, 1L, 3L, 7L, 5L, 4L, 2L, 0L, 3L, 1L, 5L, 5L, 5L, 3L, 0L)
+        println(findAMatchingOutput(program, program))
     }
 
-    private fun Int.toCombo(): ULong {
+    private fun findAMatchingOutput(program: List<Long>, target: List<Long>): Long {
+        var aStart = if (target.size == 1) {
+            0
+        } else {
+            8 * findAMatchingOutput(program, target.subList(1, target.size))
+        }
+
+        while(true) {
+            val out = runProgram(program, aStart)
+            if (out == target) {
+                println(out)
+                break
+            }
+            aStart++
+        }
+
+        return aStart
+    }
+
+    private fun Long.toCombo(a: Long, b: Long, c: Long): Long {
         return when (this) {
-            in listOf(0, 1, 2, 3) -> this.toULong()
-            4 -> regA
-            5 -> regB
-            6 -> regC
+            in 0L..3L -> this
+            4L -> a
+            5L -> b
+            6L -> c
             else -> throw IllegalArgumentException("Only 0 to 6 allowed")
         }
     }
 
-    private fun runProgram(
-        result: MutableList<ULong> = mutableListOf()
-    ) {
+    private fun runProgram(program: List<Long>, regA: Long): List<Long> {
         var instPtr = 0
+        var a = regA
+        var b = 0L
+        var c = 0L
+        val out = mutableListOf<Long>()
         while (instPtr < program.lastIndex) {
             val (opcode, operand) = program[instPtr] to program[instPtr + 1]
             when (opcode) {
-                0 -> regA = regA shr operand.toCombo().toInt()
-                1 -> regB = regB xor operand.toULong()
-                2 -> regB = operand.toCombo() % 8UL
-                3 -> if (regA != 0UL) {
-                    instPtr = operand
+                0L -> a = a shr operand.toCombo(a, b, c).toInt()
+                1L -> b = b xor operand
+                2L -> b = operand.toCombo(a, b, c) % 8L
+                3L -> if (a != 0L) {
+                    instPtr = operand.toInt()
                     continue
                 }
-                4 -> regB = regB xor regC
-                5 -> result += (operand.toCombo() % 8UL)
-                6 -> regB = regA shr operand.toCombo().toInt()
-                7 -> regC = regA shr operand.toCombo().toInt()
+                4L -> b = b xor c
+                5L -> out += operand.toCombo(a, b, c) % 8L
+                6L -> b = a shr operand.toCombo(a, b, c).toInt()
+                7L -> c = a shr operand.toCombo(a, b, c).toInt()
                 else -> throw IllegalStateException("Unknown OP Code")
             }
             instPtr += 2
         }
+        return out
     }
 }
